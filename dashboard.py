dbfe9aea7b2f58ce64061db63c2f36f2d51ffe70
TessRoovers <93520713+TessRoovers@users.noreply.github.com>
2023-07-01 16:50:03 +0200

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,109 @@
+import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+import plots
+import streamlit.components.v1 as components
+
+def main():
+    
+    st.set_page_config(
+        page_title="ESG Dashboard",
+        page_icon="./allenoveryicon.webp",
+        layout="wide",
+    )
+    
+    st.title("ESG Dashboard - Allen & Overy")
+    st.subheader("Environmental, Social and Governance")
+    st.write(" ")
+    st.markdown("Welcome to the ESG Dashboard!  \nPlease select a topic from the menu to display.")
+    st.markdown("---")
+    
+    file1 = './analysis_entities/entities_timeline.csv'
+    file2 = './analysis_words/groupedfrequencies.csv'
+
+    topics = {
+            "About": ["The Project", "Allen & Overy", "University of Amsterdam"],  
+            "ESG Entities": ["Proportional Frequencies", "Frequency & Clusters"],
+            "ESG Topics": ["Topic Analysis", "Word Frequencies"],
+            "ESG Sentiment": ["Sentiment Analysis"],
+        }
+    
+    st.sidebar.image("./allenovery.png")
+    st.sidebar.title("Menu")
+    
+    selected_option=""
+    
+    for topic, subtopics in topics.items():
+        with st.sidebar.expander(topic):
+            for subtopic in subtopics:
+                if st.button(subtopic):
+                    selected_option=subtopic
+
+    # ESG Entities
+    if "Proportional Frequencies" in selected_option:
+        st.markdown("### ESG Entities: Proportional Frequencies")
+        fig1 = plots.timeline_bar(file1)
+        st.plotly_chart(fig1)
+        st.markdown("---")
+        st.markdown("&nbsp; ")
+
+    if "Frequency & Clusters" in selected_option:
+        st.markdown("### ESG Entities: Frequency & Clusters")
+        fig2 = plots.bubble_chart(file1)
+        st.plotly_chart(fig2)
+        st.markdown("---")
+        st.markdown("&nbsp; ")
+    
+    # ESG Topics
+    if "Topic Analysis" in selected_option:
+        st.markdown("### ESG Topics: Topic Analysis")
+        year = st.selectbox("Select a year:", list(range(2013, 2024)))
+        html_string = plots.lda(year)
+        components.html(html_string, height=1200, width=1200)
+    
+    # ESG Words
+    if "Word Frequencies" in selected_option:
+        st.markdown("### ESG Topics: Word Frequencies")
+        
+        df = pd.read_csv(file2)
+        countries = df['Country'].unique()
+        country = st.selectbox('Select country:', countries)
+        filtered_data = df[df['Country'] == country]
+        years = filtered_data['Year'].unique()
+        years = [int(year) for year in years]
+        year = st.slider('Select Year', min_value=int(min(years)), max_value=int(max(years)))
+        
+        fig3 = plots.grouped_frequency(filtered_data, country, year)
+        st.plotly_chart(fig3)
+        
+        st.markdown("---")
+        st.markdown("&nbsp; ")
+    
+    # ESG Sentiment
+    if "Sentiment Analysis" in selected_option:
+        st.markdown("### ESG Sentiment: Analysis")
+        st.markdown("Coming soon.")
+        st.markdown("---")
+        st.markdown("&nbsp; ")
+    
+    # About
+    if "The Project" in selected_option:
+        st.markdown("### About: The Project")
+        st.markdown("Information on this project: coming soon.")
+        st.markdown("---")
+        st.markdown("&nbsp; ")
+        
+    if "Allen & Overy" in selected_option:
+        st.markdown("### About: Allen & Overy")
+        st.markdown("Information on Allen & Overy: coming soon.")
+        st.markdown("---")
+        st.markdown("&nbsp; ")
+    
+    if "University of Amsterdam" in selected_option:
+        st.markdown("### About: University of Amsterdam")
+        st.markdown("Information on the UvA: coming soon.")
+        st.markdown("---")
+        st.markdown("&nbsp; ")
+        
+if __name__ == "__main__":
+    main()
---
+++ plots.py
@@ -0,0 +1,273 @@
+import csv
+from collections import defaultdict
+import pandas as pd
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import plotly.express as px
+import streamlit as st
+import ast
+
+entity_names = {
+    'EPA': 'Environmental Protection Agency',
+    'BLM': 'Bureau of Land Management',
+    'Exxon': 'Exxon',
+    'FERC': 'Federal Energy Regulatory Commission',
+    'Corps': 'U.S. Army Corps of Engineers', 
+    'FWS': 'U.S. Fish and Wildlife Service',
+    'DOE': 'Department of Energy',
+    'Sierra Club': 'Sierra Club',
+    'Cal': 'California',
+    'NHTSA':'National Highway Traffic Safety Administration',
+    'Interior': 'U.S. Department of Interior',
+    'NMFS': 'National Marine Fisheries Service',
+    'Shell': 'Shell',
+    'CEQ': 'Council on Environmental Quality',
+    'the Forest Service': 'United States Forest Service'
+}
+    
+entity_classes = {
+        'EPA': 'Regulatory Authority',
+        'BLM': 'Regulatory Authority',
+        'Exxon': 'Corporate',
+        'FERC': 'Regulatory Authority',
+        'Corps': 'Regulatory Authority', 
+        'FWS': 'Regulatory Authority',
+        'DOE': 'Regulatory Authority',
+        'Sierra Club': 'NGO',
+        'Cal': 'Politics',
+        'NHTSA': 'Regulatory Authority',
+        'Interior': 'Regulatory Authority',
+        'NMFS': 'Regulatory Authority',
+        'Shell': 'Corporate',
+        'CEQ': 'Regulatory Authority',
+        'the Forest Service': 'Regulatory Authority'
+    }
+
+
+def timeline_bar(csv_file):
+    df = pd.read_csv(csv_file)
+    fig = go.Figure()
+
+    color = 'rgb(178, 52, 39, 95)'
+    totals = []
+    
+    for i, year in enumerate(df.columns[1:]):
+        total = df[year].sum()
+        totals.append(total)
+        proportions = df[year] / total
+
+        hovertext = []
+        for entity, proportion in zip(df['Entity'], proportions):
+            full_name = entity_names.get(entity, entity)
+            hover_info = f'<b>{full_name}</b></br>Proportion: {proportion:.2f}<br>Year: {year}'
+            hovertext.append(hover_info)
+
+        fig.add_trace(go.Bar(
+            x=proportions,
+            y=df['Entity'],
+            name=str(year),
+            orientation='h',
+            marker_color=color,
+            hovertemplate="%{hovertext}<extra></extra>",
+            hovertext=hovertext,
+            ),
+        )
+
+    y_axis_range = [-0.5, len(df['Entity']) - 0.5]
+
+    slider = dict(
+        active=0,
+        currentvalue={"prefix": "<b>Year:</b> "},
+        pad={"t": 50},
+        steps=[],
+        font=dict(size=12)
+    )
+
+    for i, year in enumerate(df.columns[1:]):
+        fig.update_traces(selector=dict(name=str(year)))
+        total_frequency = totals[i]
+        
+        step = dict(
+            method="update",
+            args=[
+                {"visible": [year == col for col in df.columns[1:]]},
+                {"title.text": "<b>Entity Proportional Frequencies for "+str(year)+" (total: "+str(total_frequency)+")</b>"}
+            ],
+            value=str(year),
+            label=str(year)
+        )
+        slider['steps'].append(step)
+
+    fig.update_layout(
+        barmode='group',
+        sliders=[slider],
+        height=1000,
+        width=1200,
+        margin=dict(l=100, r=100, t=100, b=100),
+        yaxis=dict(range=y_axis_range, title="<b>Name of Entity</b>", ticksuffix="    ", tickmode='array', tick0=1, dtick=1, title_font=dict(size=20), tickfont=dict(size=16), automargin=True),
+        xaxis=dict(range=[0,1], title="<b>Proportional Frequency</b>", title_font=dict(size=18), tickfont=dict(size=20)),
+    )
+    return fig
+
+
+def bubble_chart(csv_file):
+    df = pd.read_csv(csv_file)
+    frequencies = defaultdict(dict)
+
+    for _, row in df.iterrows():
+        entity = row['Entity']
+        for year in df.columns[1:]:
+            if int(year) < 2004:
+                continue
+            else:
+                frequencies[year][entity] = row[year]
+
+    data = []
+    legend_labels = list(set(entity_classes.values()))
+    class_colors = {
+        'Regulatory Authority': 'green',
+        'Corporate': 'red',
+        'NGO': 'blue',
+        'Politics': 'yellow'
+    }
+
+    for year in frequencies:
+        entities = []
+        frequencies_year = []
+        sizes = []
+        color_values = []
+
+        for entity in frequencies[year]:
+            entities.append(entity)
+            frequencies_year.append(frequencies[year][entity])
+            sizes.append(frequencies[year][entity])
+            
+            entity_class = entity_classes.get(entity)
+            if entity_class in class_colors:
+                color_values.append(class_colors[entity_class])
+
+        data.append(
+            go.Scatter(
+                x=[year] * len(entities),
+                y=entities,
+                mode='markers',
+                marker=dict(
+                    size=sizes,
+                    sizemode='diameter',
+                    sizeref=350,
+                    color=color_values,
+                    opacity=0.5,
+                    showscale=False,
+                ),
+                hovertemplate="<b>%{text}</b><br>Frequency: %{marker.size}<br>Year: %{x}<extra></extra>",
+                legendgroup='Entity Classes',
+                showlegend=False,
+                text=[entity_names.get(entity, entity) for entity in entities]
+            )
+        )
+
+    legend_traces = []
+    
+    for label in legend_labels:
+        color = class_colors[label]
+        legend_trace = go.Scatter(
+            x=[None],
+            y=[None],
+            mode='markers',
+            marker=dict(size=10, color=color),
+            showlegend=True,
+            legendgroup=label,
+            hoverinfo='none',
+            name=label,
+            opacity=0.5
+        )
+        legend_traces.append(legend_trace)
+   
+    data += legend_traces
+
+    layout = go.Layout(
+        xaxis=dict(title="<b>Year<b>", title_font=dict(size=20)),
+        yaxis=dict(title="<b>Name of Entity<b>", type='category', ticksuffix="   ", title_font=dict(size=20)),
+        height=1000,
+        width=1200,
+        margin=dict(l=100, r=100, t=100, b=100),
+        hovermode='closest',
+        showlegend=True,
+    )
+
+    fig = go.Figure(data=data, layout=layout)
+
+    fig.update_layout(
+        legend=dict(
+            itemsizing='constant',
+            title='<b> Entity Classes<b>',
+            title_font=dict(size=18),
+            bgcolor='rgba(255, 255, 255, 0.7)',
+            bordercolor='gray',
+            borderwidth=1,
+            itemclick=False,
+            itemdoubleclick=False,
+            font=dict(size=16),
+        ),
+        margin=dict(l=100, r=100, t=100, pad=10),
+    )
+
+    for trace in data[:-len(legend_traces)]:
+        fig.add_trace(trace)
+    
+    
+    return fig
+
+def lda(year):
+    with open(f'./analysis_topics/lda_vis_{year}.0.html', 'r') as f:
+        html_string = f.read()
+        
+    return html_string
+
+
+def grouped_frequency(data_frame, country, year):
+    filtered_data = data_frame[(data_frame['Country'] == country) & (data_frame['Year'] == year)]
+    frequencies = []
+    words = []
+
+    for freq_list in filtered_data['Frequency']:
+        if isinstance(freq_list, str):
+            freq_list = ast.literal_eval(freq_list)
+        
+        if isinstance(freq_list, list) and len(freq_list) > 0 and isinstance(freq_list[0], tuple) and len(freq_list[0]) == 2:
+            freq, word = zip(*freq_list)
+            frequencies.extend(freq)
+            words.extend(word)
+
+    if len(words) == 0:
+        fig = go.Figure()
+        fig.add_annotation(
+            text='No words available for the selected country and year',
+            xref='paper', yref='paper',
+            x=0.5, y=0.5,
+            showarrow=False,
+            font=dict(size=14),
+        )
+    else:
+        fig = go.Figure(
+            data=go.Bar(
+                x=words, 
+                y=frequencies, 
+                orientation='h'
+            )
+        )
+        fig.update_layout(
+            title=f'Top Words for {country} - {year}', 
+            xaxis_title='Frequency', 
+            yaxis_title='Word',
+            width=1200,
+            height=800,
+            title_font=dict(size=18),
+            font=dict(size=16)
+        )
+        
+        fig.update_traces(
+            marker=dict(color='rgb(178, 52, 39, 95)')
+        )
+
+    return fig
